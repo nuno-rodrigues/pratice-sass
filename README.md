16eb01065855d8d3b63ca37fba173a9ba4386d88
e-nrrodrigues@tap.pt <e-nrrodrigues@tap.pt>
2017-07-31 09:11:06 +0100

    Test repository name change

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Synopsis
 
-Pratice SASS
+Pratice
 https://css-tricks.com/gulp-for-beginners/
+https://css-tricks.com/reactive-uis-vanillajs-part-1-pure-functional-style/
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,24 @@
-console.log("main.js")
+//console.log("main.js")
+
+var blogPost = {
+    author: 'Brandon Smith',
+    title: 'A CSS Trick',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+};
+
+function PostPage(postData) {
+    return '<div class="page">' +
+        '<div class="header">' +
+        'Home' +
+        'About' +
+        'Contact' +
+        '</div>' +
+        '<div class="post">' +
+        '<h1>' + postData.title + '</h1>' +
+        '<h3>By ' + postData.author + '</h3>' +
+        '<p>' + postData.body + '</p>' +
+        '</div>' +
+        '</div>';
+}
+
+document.querySelector('body').innerHTML = PostPage(blogPost);
